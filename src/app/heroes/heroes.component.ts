f0ae1e30c27efcd154602fb7d479060f522149c5
juanvitofer <j.v.torres.fernandez@gmail.com>
2018-04-02 19:39:33 +0200

    Master/Detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // Refactor the component's hero property to be of type Hero
-  hero: Hero = {
-    // Initialize it with an id of 1 and the name Windstorm
-    id: 1,
-    name: 'Windstorm'
-  };
+  // Rename the component's hero property to selectedHero
+  selectedHero: Hero;
   // Add a heroes property
   heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Add the onSelect() method
+  /**
+   * Assigns the clicked hero from the template to the component's selectedHero
+   * @param hero clicked by the user
+   */
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }

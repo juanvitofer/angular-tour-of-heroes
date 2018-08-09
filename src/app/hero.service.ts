85316dde9d074a3db2a39a5b0716ef429b678686
juanvitofer <j.v.torres.fernandez@gmail.com>
2018-08-09 19:06:23 +0200

    Heroes and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,42 +1,83 @@
 import { Injectable } from '@angular/core';
+/* Import the HttpClient and HttpHeaders services */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 /* Import the Hero class */
 import { Hero } from './hero';
-/* Import the mock HEROES */
-import { HEROES } from './mock-heroes';
 /* Import the Observable and of symbols */
 import { Observable, of } from 'rxjs';
 /* Import the Message service */
 import { MessageService } from './message.service';
+/* Import the catchError symbol from rxjs/operators */
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root', // <-- Provide the service at the root level
 })
 export class HeroService {
 
-  // Add a private messageService parameter of type MessageService to the constructor
-  constructor(private messageService: MessageService) { }
+  // Define the heroesUrl with the address of the heroes resource on the server
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  // Modify the getHeroes() method to send a message when the heroes are fetched
+  // Add a private messageService parameter of type MessageService to the constructor
+  // Inject HttpClient into the constructor
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  // Modify the getHeroes() method to use HttpClient
   /**
-   * Gets the heroes from the mock heroes
+   * Gets heroes from the server
    */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    // Extend the observable result with the pipe() method and give it a catchError() operator
+    // Tap into the flow of observable values and send a message via log()
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log('fetched heroes')),
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
-  // Add the getHero() method
-  // Has an asynchronous signature
-  // Returns a mock hero as an Observable, using the RxJS of() function
+  // Modify the getHero() method to use HttpClient
   /**
-   * Get a specific hero
+   * Gets a hero by id and it will 404 if id not found
    * @param id used to get a hero
    */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    // Construct a request URL with the desired hero's id.
+    const url = `${this.heroesUrl}/${id}`;
+    // The server should respond with a single hero rather than an array of heroes
+    // Returns an Observable<Hero> rather than an observable of hero arrays
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  // Add the handleError() method
+  /**
+   * Handles Http operation that failed and lets the app continue
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  // Add the log() method
+  /**
+   * Log a HeroService message with the MessageService
+   * @param message to add in the MessageService
+   */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
